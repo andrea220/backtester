37897f7fbed1733b97ea690b7609db62dee63f89
Andrea Carapelli <carapelliandrea@gmail.com>
2025-02-15 17:44:47 +0100

    add first objects

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# backtester
-A backtester for financial derivatives.
+# Python Backtesting Library for Derivatives
+
+This is a Python-based backtesting library for financial derivatives such as equities, bonds, and options. The library allows you to define your portfolio, place trades, and calculate the value of positions using your proprietary market data, including equity prices, interest rates, and volatilities. QuantLib is used to calculate prices.
+
+## Features
+- **Portfolio Management**: Track positions and balance in your portfolio.
+- **Position Types**: Support for multiple asset types including equity, bond, and option positions.
+- **Market Data Integration**: Use a dictionary of market data to calculate the value of positions (equities, interest rates, and volatility).
+- **QuantLib Integration**: Leverage QuantLib for bond and option pricing.
+- **Trade Execution**: Ability to simulate buy and sell orders within the backtest.
---
+++ market/preprocessing.py
@@ -0,0 +1,89 @@
+import QuantLib as ql 
+import pandas as pd 
+
+DEFAULT_RISKFREE = 0.03
+
+
+
+class MarketData:
+
+    def __init__(self, start_date, end_date):
+        self.start_date = start_date
+        self.end_date = end_date
+        self.market_data = {}
+        
+    def _fill_prices(self, symbol):
+        """
+        Generate a market data dictionary from a given prices DataFrame.
+        The market data contains equity prices and dividend yields.
+        """
+        prices_df = pd.read_csv(f'data/prices/{symbol}_prices.csv').iloc[:, 1:]
+        prices_df['date'] = pd.to_datetime(prices_df['date'], format='%d/%m/%y').dt.date
+        prices_df = prices_df[(prices_df['date'] >= self.start_date)& (prices_df['date'] <= self.end_date)].copy()
+
+        # Itera sul DataFrame per riempire il dizionario market_data
+        for _, row in prices_df.iterrows():
+            current_date = row["date"]
+            
+            # Inizializza la data se non esiste già nel dizionario
+            if current_date not in self.market_data:
+                self.market_data[current_date] = {
+                    "ref_date": current_date,
+                    "equity": {},
+                    "rate": {"riskfree": DEFAULT_RISKFREE},  # Default risk-free rate
+                    "volatility": {}
+                }
+            
+            # Aggiungi i dati dell'equity
+            self.market_data[current_date]["equity"][symbol] = {
+                "price": row["price"],
+                "div_yield": row["dividend"]
+            }
+        return self.market_data
+    
+    def _fill_vols(self, symbol):
+        vols_df = pd.read_csv(f'data/volatilities/{symbol}_ivol.csv').iloc[:, 1:]
+        vols_df['reference_date'] = pd.to_datetime(vols_df['reference_date']).dt.date
+        vols_df = vols_df[(vols_df['reference_date'] >= self.start_date)& (vols_df['reference_date'] <= self.end_date)].copy()
+
+        day_count = ql.Actual365Fixed()
+        calendar = ql.TARGET()
+
+        available_dates = vols_df['reference_date'].unique()
+        for current_date in available_dates:
+            if current_date not in self.market_data:
+                self.market_data[current_date] = {
+                    "ref_date": current_date,
+                    "equity": {},
+                    "rate": {"riskfree": DEFAULT_RISKFREE},  # Default risk-free rate
+                    "volatility": {}
+                }
+            vols_df_tmp = vols_df[vols_df['reference_date'] == current_date].copy().reset_index(drop=True)
+            try:
+                spot_price = self.market_data[current_date]['equity'][symbol]['price']
+            except:
+                print(f"Cannot build a complete dataset, spot price missing on {current_date}")
+                continue
+            
+            moneyness = vols_df_tmp['moneyness'].unique()
+            strikes = (moneyness*spot_price).tolist()
+            tenors = vols_df_tmp['tenor'].unique().tolist()
+            expiration_dates = [ql.Date.from_date(current_date) + ql.Period(t) for t in tenors]
+            implied_vols = ql.Matrix(len(strikes), len(expiration_dates))
+            for i in range(implied_vols.rows()):
+                for j in range(implied_vols.columns()):
+                    implied_vols[i][j] = vols_df_tmp[(vols_df_tmp['moneyness'] == moneyness[i])&(vols_df_tmp['tenor'] == tenors[j])]['implied_vol'].values[0]
+
+            black_var_surface = ql.BlackVarianceSurface(ql.Date.from_date(current_date), calendar,
+                                                        expiration_dates, strikes,
+                                                        implied_vols, day_count)
+            black_var_surface.setInterpolation("bicubic")
+            black_var_surface.enableExtrapolation()
+
+            self.market_data[current_date]['volatility'][symbol] = black_var_surface
+    
+    def build(self, symbol):
+        self._fill_prices(symbol)
+        self._fill_vols(symbol)
+
+
